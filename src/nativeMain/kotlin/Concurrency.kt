aedd2492eb877f297bd72449cae185f2e6f13002
Vyacheslav Kormushkin <Vyacheslav.Kormushkin@Harman.com>
2021-01-25 11:10:13 +0300

    Concurrency exaples update

--- src/nativeMain/kotlin/Concurrency.kt
+++ src/nativeMain/kotlin/Concurrency.kt
@@ -1,30 +1,44 @@
 import kotlinx.coroutines.*
+import kotlin.native.concurrent.TransferMode
+import kotlin.native.concurrent.Worker
 
 fun mutateWithinCoroutine () {
 
     val someData = SomeData("test")
 
     runBlocking {
-       GlobalScope.launch {
-            someData.name = "new" //Exception in case of multithreaded coroutines
-        }.join()
+        launch(Dispatchers.Unconfined) {
+            someData.name = "new1" //No exception
+        }
+        println(someData.name)
+        launch(Dispatchers.Default) {
+            someData.name = "new2" //Exception because coroutine freezes input block on thread switch:
+           //https://github.com/Kotlin/kotlinx.coroutines/blob/9eb3543aa6811d7bbfd7bbc9f7a2389f61e7c716/kotlinx-coroutines-core/native/src/Builders.kt#L130
+        }
     }
 }
 
-suspend fun giveSomeData() : SomeData{
+fun giveSomeData() : SomeData{
     return SomeData("2")
 }
 
 fun mutateAsyncOutput () {
-
-    var someData = SomeData("1")
+    var someData: SomeData? = null
 
     runBlocking {
-        someData = async {
+        someData = withContext(Dispatchers.Default) {
             giveSomeData()
-        }.await()
+        }
     }
 
-    someData.name = "3" //I would expect an exception here but it does not happen
-
+    someData?.name = "new" //Exception
 }
+
+fun mutateWithWorker() {
+    val someData = SomeData("test")
+    val worker = Worker.start()
+    worker.execute(TransferMode.SAFE, {someData}) {
+        it.name = "new" //This causes IllegalStateException unless freeze() is called on a producer
+    }
+}
+
--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -4,6 +4,7 @@ data class SomeData(var name: String)
 
 fun main() {
     try {
+        Platform.isMemoryLeakCheckerActive = false //prevent worker leaking error
     } catch (e: InvalidMutabilityException) {
         println("************")
         println("InvalidMutabilityException")
@@ -11,6 +12,8 @@ fun main() {
         return
     } catch (e : Exception) {
         println("Some other exception")
+        println(e)
+        return
     }
 
     println("No Exception")}
